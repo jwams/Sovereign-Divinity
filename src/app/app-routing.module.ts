4a725c07bbe1cb2d76c5dd1aabc0b6aea2882d18
jwams <justin.wams@csc-scc.gc.ca>
2018-12-28 13:55:00 -0800

    Initial push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+// Angular Imports
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Page Imports
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { TestpageComponent } from './pages/testpage/testpage.component';
+
+const routes: Routes = [
+	{ path: 'homepage', component: HomepageComponent },
+	{ path: 'testpage', component: TestpageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { TestpageComponent } from './pages/testpage/testpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    TestpageComponent
   ],
   imports: [
     BrowserModule,
